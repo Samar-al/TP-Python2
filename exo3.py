6b33b6811e2f2de6577a0d0f407c5ed829af8631
Samar-Al <samar.alkhalil@gmail.com>
2025-07-08 11:26:08 +0200

    adding function and lambdas

--- exo3.py
+++ exo3.py
@@ -1,14 +1,20 @@
 import main
 
-grain = 1
-total = 0
-exponent = 0
+def display_chessboard_grains():
+    grain = 1
+    total = 0
+    exponent = 0
 
-for row in range(1, 9):
-    for col in range(1, 9):
-        print(f"Ligne {row}, Colonne {col} : 2^{exponent:<2} = {grain:>22,} grain(s)")
-        total += grain
-        grain *= 2
-        exponent += 1
-print("\nTotal de grains de riz sur l’échiquier :", total)
-print(f"Ce qui équivaut à environ : {total:.2e} grains")
+    format_number = lambda n: f"{n:>22,}"
+    format_scientific = lambda n: f"{n: .2e}"
+
+    for row in range(1, 9):
+        for col in range(1, 9):
+            print(f"Ligne {row}, Colonne {col} : 2^{exponent:<2} = {format_number(grain)} grain(s)")
+            total += grain
+            grain *= 2
+            exponent += 1
+    print("\nTotal de grains de riz sur l’échiquier :", format_number(total))
+    print("Ce qui équivaut à environ :", format_scientific(total), "grains")
+
+display_chessboard_grains()
