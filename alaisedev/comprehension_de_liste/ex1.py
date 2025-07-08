df79ddcca179bf966d76ac4a02abbf66a78797a8
Samar-Al <samar.alkhalil@gmail.com>
2025-07-08 16:07:00 +0200

    exo1,2,3,5

---
+++ alaisedev/comprehension_de_liste/ex1.py
@@ -0,0 +1,6 @@
+# Génère une liste des carrés des nombres de 0 à 9
+
+squares = [x**2 for x in range(10)]
+print(squares)
+
+
---
+++ alaisedev/comprehension_de_liste/ex5.py
@@ -0,0 +1,7 @@
+# crée une liste de liste
+# pour les tables de multiplications de 1 à 10
+# [[1,2,3,...], [2,4,6,...],...]
+
+muliplication = [[x * y for x in range(1, 11)] for y in range(1, 11)]
+
+
